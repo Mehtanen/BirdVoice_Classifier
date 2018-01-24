1c5f57c294ccbb48c2ad1dc25b5630bfe3601ea2
Mehtanen <33399288+Mehtanen@users.noreply.github.com>
2018-01-24 13:16:23 +0200

    Update poista_turhat_pisteet.m

--- poista_turhat_pisteet.m
+++ poista_turhat_pisteet.m
@@ -1,3 +1,4 @@
+% Remove extra dots when new class has been established
 function [contClassifiedStuff] = poista_turhat_pisteet(contClassifiedStuff)
         d = 1;    
     if strcmp(contClassifiedStuff(d).name,'.')== 1 % tulee 1 jos samat
