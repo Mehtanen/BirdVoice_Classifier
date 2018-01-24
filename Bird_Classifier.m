f1c231d7da16fe7996c1dbfe330575d52b414cc2
Mehtanen <33399288+Mehtanen@users.noreply.github.com>
2018-01-24 13:14:19 +0200

    Update Bird_Classifier.m

--- Bird_Classifier.m
+++ Bird_Classifier.m
@@ -1,91 +1,110 @@
 function Bird_Classifier
+
+% Bird voice classifier is the software that classifies bird voices into the different classes.
+% Classifier is based on the idea of unsupervised classification. Cheak README file for comprehensive 
+% information of the system.
+
 %% Prepare path and file
 
 % From where to read
 pathFrom = 'C:\Users\Tero\Desktop\Mathlab\Machinelearning\Fidecula\';
 ident = '*.wav';
 
-% Construct wav_path in order to provide it to audioreader later 
+% Construct wav_path to provide it to audioreader later 
 result = horzcat(pathFrom,ident);
 wav_path = dir(result);
 
 % Where to create output files
 pathTo = 'C:\Users\Tero\Desktop\Mathlab\Machinelearning\ClassifiedStuff\';
 
-% If file exist delete pathTo and its content
+% If file exists delete it and all its content
 if length(dir(pathTo)) > 50
      rmdir ClassifiedStuff s
      pause(60);
 end
 
-% Create folder to pathTo path
+% Create target folder to pathTo 
 mkdir('ClassifiedStuff');
 f = fullfile('C:\Users\Tero\Desktop\Mathlab\Machinelearning\','ClassifiedStuff');
 save(f)
 
 %% Loop through files and test them against references
-% Initial this to true to allow adding the first reference to the list 
+
+% Initially this is true to allow adding the first reference to the reference list 
 r = true;
+
+% These could be in the same list but I didn't spend extra time to figuring out syntax for that
 referenssit = {};
 s_refeaanet = struct('name',{},'date',{},'bytes',{},'isdir',{},'datenum',{});
 curve_lista = {};
 
-for t = 2:length(wav_path) % n‰‰ on testattavia
+% Loop through the original list of the voices
+for t = 2:length(wav_path)
 
-    testattava = wav_path(t).name; % t‰t‰ pit‰is vertailla aiempiin referensseihin 
-    y1 = audioread([pathFrom wav_path(t).name]);
-    curve1 = harmonic_frequency_seek(y1);
+    testattava = wav_path(t).name; 
+    y1 = audioread([pathFrom wav_path(t).name]); % Read audiofile
     
-    % Seuraavien rivien listat olisi hyv‰ yhdist‰‰. Referenssilistan p‰ivitt‰miseen 
-    % perustuva koodi tuli rakennettua ensimm‰isen‰, joten lis‰ilin j‰lkik‰teen kaksi 
-    % muuta listaa. L‰hinn‰ laskennan nopeuttamiseksi
+ % harmonic_frequency_seek-function was provided for me from Tampere University of Technology.  
+    curve1 = harmonic_frequency_seek(y1);        % Calculate harmonic frequencies
     
-    if r == true % perustettiinko uusi luokka, jos perustettiin kasvatetaan ref listaa
-       referenssit{end+1} = wav_path(t-1).name;   % t-1 hakee aiemman ‰‰nen 
+    % If decision maker established a new class this will be true
+    if r == true 
+       referenssit{end+1} = wav_path(t-1).name;  
        referenssit = referenssit';
        
        y = audioread([pathFrom wav_path(t-1).name]);
+
        curve = harmonic_frequency_seek(y);
        
        curve_lista{end+1} = curve;    
        s_refeaanet(end+1) = wav_path(t-1);
        
     end
-
+    
+% Loop through reference list to test against everyone of them
   for refe = 1:length(referenssit)
-        curve = curve_lista{1,refe}(1,:); % hakee referenssi kurvin
+      
+      % Get reference curve
+        curve = curve_lista{1,refe}(1,:);
+        
+      % Execute Dynamic Time Warping to calculate difference between reference curve and the curve to be tested  
         diag = dtw(curve,curve1);
-        list(refe,:) = [referenssit(refe),diag]; % t‰‰ll‰ on aina viimeinen referenssi‰ vastaava dist
-        r = false; % boolean nollaus
+        list(refe,:) = [referenssit(refe),diag]; 
+        r = false; % reset boolean
   end
+  
 %% Decision maker
 
-    % jos referenssi‰ ei lˆydy ja ovat samanlaisia, tehd‰‰n kansio ja
-    % laitetaan molemmat sinne
+    % If reference curve is not found new class will be established and
+    % file will be added there
     [what_class, amount_of_classes] = FileReader_Output(referenssit(refe),pathTo);
-    if strcmp(what_class,'ei asettunut') == 1 % nyt tehd‰‰n uusi luokka, jos referenssi‰ ei lˆydy               
-        FileWriter(pathTo,what_class,amount_of_classes,referenssit); % luo kansion ja lis‰‰ sinne 
+    if strcmp(what_class,'ei asettunut') == 1              
+        FileWriter(pathTo,what_class,amount_of_classes,referenssit); 
     end
     
-    % saa lis‰t‰ luokkaan jos listan pienin ref diag arvo on alle rajan
-    listan_diagit = list(:,2);      % kaikki diagit
-    d = cell2mat(listan_diagit);    % muunnos      
-    [M,I] = min(d);                 % indeksi, josta pienin lˆytyi
+    % The system is allowed to add a file to the certain class if
+    % difference between reference of class and voice to be tested is small enough
+    listan_diagit = list(:,2);      
+    d = cell2mat(listan_diagit);       
+    [M,I] = min(d);                 
     
-    % indeksill‰ luokka mihin lis‰t‰‰n
+    % Index of the class to be added
     listan_referenssit = list(:,1);
     refer = cell2mat(listan_referenssit(I));
     
+    % M is warping limit. Smaller value will eventually lead to greater number of classes, 
+    % because it raises standards to be "similar" voice 
     if M < 7000
           class = FileReader_Output(refer,pathTo); 
           FileWriter(pathTo,class,amount_of_classes,testattava);
     else
-      what_class = 'ei asettunut'; % antaa luvan luoda uusi kansio
-      FileWriter(pathTo,what_class,amount_of_classes,testattava); % luo kansion ja lis‰‰ testattavan sinne
+      what_class = 'ei asettunut';                                % allows establishing the new class
+      FileWriter(pathTo,what_class,amount_of_classes,testattava); % creates new class and adds file there
       r = true; 
 
     end
 end
+
 %% Plot histogram   
 
     content_Classfied_Folder = poista_turhat_pisteet(dir(pathTo));
@@ -105,6 +124,13 @@ end
     title('Voices in class');
     
 end
+
+
+
+
+
+
+
 
 
 
